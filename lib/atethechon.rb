0c40c73a5d89e27ae4afec277ca075722070de89
David Wilkie <dwilkie@gmail.com>
2022-10-12 12:48:28 +0700

    Initial commit

---
+++ lib/atethechon.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "atethechon/version"
+
+module Atethechon
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/atethechon/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Atethechon
+  VERSION = "0.1.0"
+end
